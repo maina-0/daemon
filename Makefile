136318f5a5fc39203a5f25bca17540e1055dad4c
maina-0 <maina0@proton.me>
2025-01-18 19:19:52 +0300

    roy

--- Makefile
+++ Makefile
@@ -1,13 +1,13 @@
 # _*_ MakeFile _*_
 #
 CC = clang
-CFLAGS = -g -Wall
+CFLAGS = -g -Wall -v
 A_CFLAGS = -c
 OBJ = $(wildcard *.o)
 
 
-main: main.o pid.o sid.o
-	$(CC) $(CFLAGS) main.o pid.o sid.o -o main
+main: main.o pid.o sid.o sig.o
+	$(CC) $(CFLAGS) main.o pid.o sid.o sig.o -o main
 
 main.o: main.c
 	 $(CC) $(CFLAGS) $(A_CFLAGS) main.c
@@ -17,6 +17,10 @@ sid.o: sid.c
 
 pid.o: pid.c
 	$(CC) $(CFLAGS) $(A_CFLAGS) pid.c
+
+sig.o: sig.c
+	$(CC) $(CFLAGS) $(A_CFLAGS) sig.c
+
 
 
 .PHONY: clean
--- main.c
+++ main.c
@@ -1,19 +1,30 @@
 #include <stdio.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
 
 int pid();
 int sid();
+void sig(int signo);
 
 int main(){
     pid();
     umask(0);
     sid();
-    close(STDIN_FILENO);
-    close(STDOUT_FILENO);
-    close(STDERR_FILENO);
+ //   close(STDIN_FILENO);
+ //   close(STDOUT_FILENO);
+ //   close(STDERR_FILENO);
 
-    printf("hello\n");
+    if (signal(SIGINT,sig)==SIG_ERR){
+        perror("an error occurred, didn't quite catch your signal:");
+        exit(1);
+    }
+
+     while (1) {
+        sleep(1);
+    }
+
 
 
     return 0;
